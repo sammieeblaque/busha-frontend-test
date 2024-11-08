7e71716ae70ad8cceacc3e3688a0d1bb1ffe30b9
Sammiee <sammieeblaque@users.noreply.github.com>
2024-11-08 10:23:46 +0100

    chore: add content modal

--- src/components/shared/style.ts
+++ src/components/shared/style.ts
@@ -41,3 +41,86 @@ export const GridContainer = styled.div`
     grid-template-columns: repeat(3, 1fr); /* Equivalent to lg:grid-cols-3 */
   }
 `;
+
+// Modal
+
+export const ModalInnerContainer = styled.div`
+  padding-left: 1.75rem; /* Equivalent to px-7 */
+  padding-right: 1.75rem; /* Equivalent to px-7 */
+  padding-top: 3rem; /* Equivalent to py-[80px] */
+  padding-bottom: 0;
+`;
+
+export const ModalHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const ModalTitle = styled.h3`
+  font-size: 1.5rem; /* Equivalent to text-[24px] */
+  color: black;
+`;
+
+export const CloseButton = styled.span`
+  cursor: pointer;
+`;
+
+export const Description = styled.div`
+  margin-top: 1.25rem; /* Equivalent to mt-5 */
+  display: flex;
+  align-items: center;
+`;
+
+export const DescriptionText = styled.p`
+  font-size: 0.9375rem; /* Equivalent to text-[15px] */
+  font-weight: 400;
+  color: #3e4c59; /* Equivalent to text-[#3E4C59] */
+`;
+
+export const SelectContainer = styled.div`
+  margin-top: 1.25rem; /* Equivalent to mt-5 */
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem; /* Equivalent to gap-3 */
+`;
+
+export const Label = styled.label`
+  font-size: 1rem; /* Equivalent to text-[16px] */
+  color: #3e4c59; /* Equivalent to text-[#3E4C59] */
+`;
+
+export const Select = styled.select`
+  height: 4rem; /* Equivalent to h-[64px] */
+  width: 100%; /* Full width */
+  border-radius: 0.25rem; /* Equivalent to rounded-sm */
+  border: 1px solid #cbd2d9; /* Equivalent to border-[#CBD2D9] */
+  padding-left: 0.75rem; /* Equivalent to px-3 */
+  padding-right: 0.75rem; /* Equivalent to px-3 */
+  cursor: pointer;
+  font-weight: 400;
+  font-size: inherit;
+`;
+
+export const ButtonWrapper = styled.div`
+  margin-top: 1.25rem; /* Equivalent to mt-5 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ModalButton = styled.button`
+  height: 3.375rem; /* Equivalent to h-[54px] */
+  width: 15rem; /* Equivalent to w-[180px] */
+  border-radius: 9999px; /* Equivalent to rounded-full */
+  background-color: black;
+  color: white;
+  border: none;
+  cursor: pointer;
+  margin-top: 5px;
+
+  &:disabled {
+    background-color: #cccccc; /* Disabled state color */
+    cursor: not-allowed;
+  }
+`;
